b8f674589f9aae74a4f728e098189691a740ed8e
Jay Arredondo <jearredondo13@gmail.com>
2020-09-09 12:28:03 -0500

    added environment variables to make more requests to the github api

--- src/App.js
+++ src/App.js
@@ -2,17 +2,31 @@ import React, {Fragment,Component} from 'react';
 import Navbar from './components/layout/Navbar';
 import UserItem from './components/users/UserItem'
 import Users from './components/users/Users'
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-    render() {
 
+    state = {
+        users: [],
+        loading: false
+    };
+
+    // used for http requests
+    async componentDidMount() {
+        this.setState({loading: true})
+        const res = await axios
+            .get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+        this.setState({users: res.data, loading: false})
+    }
+
+    render() {
         return (
             //  can use <fragment> as a ghost element, meaning it won't appear in the dom.
             <div className="App">
                 <Navbar title={'GitHub Finder'} icon={'fab fa-github'}/>
                 <div className="container">
-                <Users/>
+                <Users loading={this.state.loading} users={this.state.users}/>
                 </div>
             </div>
             // </fragment>
